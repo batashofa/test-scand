8109206aea18e0885a3d63a7b06456506b49b152
batashofa <Vegavetta1424>
2023-04-14 09:59:14 +0300

    feat: added burger menu in header

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 import "./style/style.scss";
 import "./fonts/fonts.scss"
 import "@fontsource/manrope";
+import $ from "jquery";
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.header__burger-menu_button', '.header__burger-menu_lines');
+    let links = menu.find('.header__burger-menu_link');
+    let overlay = menu.find('.header__burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu(){
+        menu.toggleClass('header__burger-menu_active');
+
+        if (menu.hasClass('header__burger-menu_active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.header__burger-menu');
